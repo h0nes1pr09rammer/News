613e1b82922b27cf108d18e679273aeb79c6108e
h0nes1pr09rammer <15824444354@163.com>
2016-07-27 17:58:18 +0800

    使用Imageload加载图片

---
+++ app/src/main/java/com/ctwl/lzq/howmuchanimation/Utils/CommonUtils.java
@@ -0,0 +1,7 @@
+package com.ctwl.lzq.howmuchanimation.Utils;
+
+/**
+ * Created by B41-80 on 2016/7/27.
+ */
+public class CommonUtils {
+}
---
+++ app/src/main/java/com/ctwl/lzq/howmuchanimation/db/NewsDataBaseHelper.java
@@ -0,0 +1,7 @@
+package com.ctwl.lzq.howmuchanimation.db;
+
+/**
+ * Created by h0nes1pr09rammer on 2016/7/27.
+ */
+public class NewsDataBaseHelper {
+}
