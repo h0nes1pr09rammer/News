95723cfb8e3eac448cf27f45f6115899df9b6e0a
h0nes1pr09rammer <15824444354@163.com>
2016-08-03 17:49:25 +0800

    增加数据库存储数据

---
+++ app/src/main/java/com/ctwl/lzq/howmuchanimation/Adapter/BaseAdapter.java
@@ -0,0 +1,7 @@
+package com.ctwl.lzq.howmuchanimation.Adapter;
+
+/**
+ * Created by h0nes1pr09rammer on 2016/8/2.
+ */
+public class BaseAdapter {
+}
---
+++ app/src/main/java/com/ctwl/lzq/howmuchanimation/listener/RecyclerViewScrollListener.java
@@ -0,0 +1,7 @@
+package com.ctwl.lzq.howmuchanimation.listener;
+
+/**
+ * Created by h0nes1pr09rammer on 2016/8/3.
+ */
+public class RecyclerViewScrollListener {
+}
